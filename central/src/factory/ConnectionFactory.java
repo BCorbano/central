5762eaa32c4ce6c050e0217099c842080e721f3c
BCorbano <42554884+BCorbano@users.noreply.github.com>
2018-11-05 17:32:11 -0200

    Add files via upload

---
+++ central/src/factory/ConnectionFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory; 
+ 
+// faz as importações de classes necessárias para o funcionamento do programa 
+import java.sql.Connection; 
+// conexão SQL para Java 
+import java.sql.DriverManager; 
+// driver de conexão SQL para Java 
+import java.sql.SQLException; 
+
+public class ConnectionFactory {
+    public Connection getConnection(){
+        try {
+		return DriverManager.getConnection("jdbc:mysql://localhost/projetojava","aw1","turma320");
+	 }         
+	 catch(SQLException excecao) {
+		throw new RuntimeException(excecao);
+	}
+    }
+    
+}
---
+++ central/src/features/Bloco.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package features;
+
+
+
+
+/**
+ *
+ * @author aluno
+ */
+public class Bloco extends telas.TelaCriaBloco {
+    public static void main(String[] args){
+        
+        String url;
+        String tamanho;
+    }
+    
+}
